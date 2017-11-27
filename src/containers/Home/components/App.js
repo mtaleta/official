54ba171a95ae495d07db7005e82e165c6296db6b
mtal <ex051240@gmail.com>
2017-11-27 22:53:22 +0800

    First pages commit

--- src/containers/Home/components/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Todolist from './Todolist';
-
-export default class App extends React.Component {
-  render() {
-    return <Todolist initText="開始輸入文字吧！" />;
-  }
-}
